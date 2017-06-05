542083ebd52bd739c09a39e02f049e40fd96814d
ramakrishna s <ramakrishna.s@jarustech.com>
2017-06-05 17:25:56 +0530

    server creation using node
    server creation using node

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-
+const express = require('express');
+const app = express();
+app.get('/',function(req, res){
+	res.send("Hello world example");
+})
+app.listen(3000,function(){
+	console.log("Server is running on port number 3000");
+})
